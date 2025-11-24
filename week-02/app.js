d771958d3f4d0cf8d42989d5b3228d1a80158d50
freda manu <fredaabena5@gmail.com>
2025-11-24 14:59:51 +0000

    create a console

---
+++ week-02/app.js
@@ -0,0 +1,30 @@
+console.log("Hello world!");
+
+const blogPosts = [
+  "my first blog post",
+  "my second blog post",
+  "my third blog post",
+];
+console.log(blogPosts);
+
+console.log(blogPosts[0]);
+console.log(blogPosts[1]);
+console.log(blogPosts[2]);
+
+blogPosts[3] = "My fourth blog post";
+
+console.log(blogPosts[3]);
+
+const favouriteFoods = ["burger", "fried rice", "jollof rice"];
+
+const favouriteColours = ["Purple", "turquoise", "red"];
+
+const favouriteNumbers = [7, 12, 15];
+
+console.log(favouriteFoods);
+console.log(favouriteColours);
+console.log(favouriteNumbers);
+
+console.log(favouriteFoods[2]);
+console.log(favouriteColours[0]);
+console.log(favouriteNumbers[2]);
